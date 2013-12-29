cd37ea14c909431d598c086e1763fa582e1e106f
morl99 <choffmann87@gmail.com>
2013-12-29 16:57:35 +0100

    Refactored out all actual logWriting
    Preparation for another LogWriter Class (CsvLogWriter)

---
+++ src/edu/umich/PowerTutor/service/TextLogWriter.java
@@ -0,0 +1,62 @@
+package edu.umich.PowerTutor.service;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Map;
+
+import edu.umich.PowerTutor.phone.PhoneConstants;
+import edu.umich.PowerTutor.util.BatteryStats;
+import edu.umich.PowerTutor.util.NotificationService;
+import edu.umich.PowerTutor.util.SystemInfo;
+
+public class TextLogWriter {
+  private OutputStreamWriter logStream;
+
+  public TextLogWriter(OutputStreamWriter logStream) {
+    this.logStream = logStream;
+  }
+
+  public void writeIterationHeader(long iter, int totalPower) throws IOException {
+    logStream.write("begin " + iter + "\n");
+    logStream.write("total-power " + (long) Math.round(totalPower) + '\n');
+  }
+
+  public void writeSingleDataPoint(String name, int uid, PowerData powerData) throws IOException {
+    long roundedCachePower = (long) Math.round(powerData.getCachedPower());
+    if (uid == SystemInfo.AID_ALL) {
+      logStream.write(name + " " + roundedCachePower + "\n");
+      powerData.writeLogDataInfo(logStream);
+    } else {
+      logStream.write(name + "-" + uid + " " + roundedCachePower + "\n");
+    }
+  }
+
+  public void writeMemInfo(long[] memInfo) throws IOException {
+    logStream.write("meminfo " + memInfo[0] + " " + memInfo[1] + " " + memInfo[2] + " " + memInfo[3] + "\n");
+  }
+
+  public void writeFirstIteration(BatteryStats bst, PhoneConstants phoneConstants, Map<Integer, String> uidAppIds)
+      throws IOException {
+
+    logStream.write("time " + System.currentTimeMillis() + "\n");
+    Calendar cal = new GregorianCalendar();
+    logStream.write("localtime_offset " + (cal.get(Calendar.ZONE_OFFSET) + cal.get(Calendar.DST_OFFSET)) + "\n");
+    logStream.write("model " + phoneConstants.modelName() + "\n");
+    if (NotificationService.available()) {
+      logStream.write("notifications-active\n");
+    }
+    if (bst.hasFullCapacity()) {
+      logStream.write("batt_full_capacity " + bst.getFullCapacity() + "\n");
+    }
+    synchronized (uidAppIds) {
+      for (int uid : uidAppIds.keySet()) {
+        if (uid < SystemInfo.AID_APP) {
+          continue;
+        }
+        logStream.write("associate " + uid + " " + uidAppIds.get(uid) + "\n");
+      }
+    }
+  }
+}
